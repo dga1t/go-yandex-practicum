ca183d39476b27d0e982e0b1ef064fd553580794
Dima Pasumansky <dpsmnsk@gmail.com>
2023-01-03 17:08:37 +0500

    upd

--- hello.go
+++ hello.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 	// "time"
 )
 
@@ -218,4 +221,23 @@ func ReadResponse(rawResp string) (Response, error) {
 	}
 
 	return resp, nil
+}
+
+// exercise #1 in functions
+func PrintAllFilesWithFilter(path string, filter string) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("unable to get list of files", err)
+		return
+	}
+
+	for _, f := range files {
+		filename := filepath.Join(path, f.Name())
+		if strings.Contains(filename, filter) {
+			fmt.Println(filename)
+		}
+		if f.IsDir() {
+			PrintAllFilesWithFilter(filename, filter)
+		}
+	}
 }
